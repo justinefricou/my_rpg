727b9f933adf128ade935d5b0643dff7c846a1c9
Justine Fricou <jfricou@justine.fricou>
2020-04-22 16:50:12 +0200

    [Add] save_clues.c, load_save_clues.c : empty functions to be coded later

---
+++ src/save_load/save_clues.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_rpg_2019
+** File description:
+** Saves the current progression regarding clues.
+*/
+
+#include <stdio.h>
+#include "save_load.h"
+
+void save_clues(clues_t clues, FILE *save_file)
+{
+    // TODO
+}

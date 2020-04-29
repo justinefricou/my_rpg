df9be93b351a2b768752c9b09b553dedc16a81ea
Justine Fricou <jfricou@justine.fricou>
2020-04-29 20:48:57 +0200

    [Modify] general_data.h : Adding dialogs to general_data

--- include/general_data.h
+++ include/general_data.h
@@ -9,6 +9,7 @@
 #include "libdragon.h"
 #include "event.h"
 #include "map.h"
+#include "init_dialogs.h"
 
 #ifndef GENERAL_DATA_H_
 #define GENERAL_DATA_H_
@@ -107,6 +108,7 @@ typedef struct general_data
     object_list_t object_list;
     event_manager_t event_manager;
     map_t maps;
+    dialog_t dialogs;
     options_t options;
     int block_input;
     set_animator_t sprite_id[SPRITE_ID_LEN];

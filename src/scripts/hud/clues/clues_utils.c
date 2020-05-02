943e97121679a9abe4bc87066efae9a3502658e1
Justine Fricou <jfricou@justine.fricou>
2020-05-03 01:22:02 +0200

    [Add] scripts for librarian, book1, book2

--- src/scripts/hud/clues/clues_utils.c
+++ src/scripts/hud/clues/clues_utils.c
@@ -13,38 +13,57 @@
 
 static const char *CLUES_DESCRIPTION[CLUES_LEN][20] = {
     {
-        "According to the photographer, there was the sound of a chainsaw\n" \
-        "during a long time. Then, screams (male and female voices) and\n" \
-        "the chainsaw stopped.",
+        "According to the photographer, there was the sound of a chainsaw",
+        "during a long time. Then, screams (male and female voices) and",
+        "then the chainsaw stopped.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "Use the right and left arrows to navigate though the clues"
+    },
+    {
         "The body has been cut in half using the logger's chainsaw.",
+    },
+    {
         "According to the logger, his chainsaw was stolen and used as the\n" \
         "weapon so people would think he is the murderer.",
+    },
+    {
         "According to the policeman, the photographer is the murderer.\n"\
         "He loves the forest and didn't want the campsite's owner to destroy\n"\
         "it to expand his business, so he killed him.",
+    },
+    {
         "According to the logger, the photographer isn't the murderer. "\
         "Indeed,\nthey are allies against the campsite's owner and the shop's\n"\
         "owner in th battle for the forest, and the photographer wouldn't\n"\
         "have used a weapon that would incriminate his ally (his chainsaw).",
+    },
+    {
         "According to the shop owner, the campsite owner was his ally against"\
         "\nthe photographer and the logger, and he wouldn't kill his ally.",
+    },
+    {
         "According to the cashier, the shop's owner has no scruples when\n"\
         "it comes to expanding his business and earning more money. The\n"\
         "cashier thinks he killed hiw own ally, the campsite owner, so that\n"\
         "people would suspect the logger and the photographer, and so that\n"\
         "he would get all the land to himself.",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
+    },
+    {
         "That's all, go talk to the villagers to find more clues."
     }
 };

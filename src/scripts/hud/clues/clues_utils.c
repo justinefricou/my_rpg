bd7d743d26c97433e67161e385f4d539067c7593
Justine Fricou <jfricou@justine.fricou>
2020-05-02 22:41:14 +0200

    [ADD] dialogs and cluesfor cashier, shop owner, logger, mayor, photographer, policeman

--- src/scripts/hud/clues/clues_utils.c
+++ src/scripts/hud/clues/clues_utils.c
@@ -13,8 +13,26 @@
 
 static const char *CLUES_DESCRIPTION[CLUES_LEN][20] = {
     {
-        "This is a clue, but the game hasn't been made",
-        "so there is no clues ?",
+        "According to the photographer, there was the sound of a chainsaw\n" \
+        "during a long time. Then, screams (male and female voices) and\n" \
+        "the chainsaw stopped.",
+        "The body has been cut in half using the logger's chainsaw.",
+        "According to the logger, his chainsaw was stolen and used as the\n" \
+        "weapon so people would think he is the murderer.",
+        "According to the policeman, the photographer is the murderer.\n"\
+        "He loves the forest and didn't want the campsite's owner to destroy\n"\
+        "it to expand his business, so he killed him.",
+        "According to the logger, the photographer isn't the murderer. "\
+        "Indeed,\nthey are allies against the campsite's owner and the shop's\n"\
+        "owner in th battle for the forest, and the photographer wouldn't\n"\
+        "have used a weapon that would incriminate his ally (his chainsaw).",
+        "According to the shop owner, the campsite owner was his ally against"\
+        "\nthe photographer and the logger, and he wouldn't kill his ally.",
+        "According to the cashier, the shop's owner has no scruples when\n"\
+        "it comes to expanding his business and earning more money. The\n"\
+        "cashier thinks he killed hiw own ally, the campsite owner, so that\n"\
+        "people would suspect the logger and the photographer, and so that\n"\
+        "he would get all the land to himself.",
         "",
         "",
         "",
@@ -27,12 +45,7 @@ static const char *CLUES_DESCRIPTION[CLUES_LEN][20] = {
         "",
         "",
         "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "This is the end, please fullfill clues."
+        "That's all, go talk to the villagers to find more clues."
     }
 };
 

3e5af54ba18a1784d1c5ab99efd4f681718b6080
Vitor Hugo Moreira Reis <vreis@vreis.FedoraVM3>
2020-05-03 15:07:54 +0200

    [ADD] energy drink added to refill the character's energy
    Committer: Vitor Hugo Moreira Reis <vitor-hugo.moreira-reis@epitech.eu>

---
+++ src/other/objects/energy_drink.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2019
+** rpg
+** File description:
+** objects
+*/
+
+#include "object.h"
+#include "libdragon.h"
+#include "general_data.h"
+
+static void e_drink_action(dg_window_t *w)
+{
+    general_data_t *gd = w->general_data;
+
+    gd->player.pm.x += 10;
+    if (gd->player.pm.x > gd->player.pm.y)
+        gd->player.pm.x = gd->player.pm.y;
+}
+
+object_t energy_drink(void)
+{
+    object_t obj = {0};
+
+    obj.name = "calming_pills";
+    obj.description = "Restores some of your energy.\n+10pm";
+    obj.icon_id = 2;
+    obj.consomable = 1;
+    obj.action = &e_drink_action;
+    obj.price = 8;
+    return obj;
+}

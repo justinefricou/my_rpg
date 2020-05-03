1c373867750d63b03f44845e4995683e98e859f7
Justine Fricou <jfricou@justine.fricou>
2020-05-03 15:18:02 +0200

    [Add] scripts and clues for barman, book1, child1, fisher, librarian, logger, mother1, mother2, policeman

--- src/scripts/hud/clues/clues_utils.c
+++ src/scripts/hud/clues/clues_utils.c
@@ -18,7 +18,7 @@ static const char *CLUES_DESCRIPTION[CLUES_LEN][20] = {
     {
         "According to the photographer, there was the sound of a chainsaw",
         "during a long time. Then, screams (male and female voices) and",
-        "the chainsaw stopped.",
+        "then the chainsaw stopped.",
         "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
         "Use the right and left arrows to navigate through the clues"
     },
@@ -28,9 +28,11 @@ static const char *CLUES_DESCRIPTION[CLUES_LEN][20] = {
         "Use the right and left arrows to navigate through the clues"
     },
     {
-        "According to the logger, his chainsaw was stolen and used as the",
-        "weapon so people would think he is the murderer.",
-        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "According to the logger, his chainsaw was stolen and turned on for",
+        "a long time before being used as the weapon so people would think",
+        "he was cutting wood when he saw Rob, the campsite owner, and",
+        "killed him",
+        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
         "Use the right and left arrows to navigate through the clues"
     },
     {
@@ -57,10 +59,77 @@ static const char *CLUES_DESCRIPTION[CLUES_LEN][20] = {
     {
         "According to the cashier, the shop's owner has no scruples when",
         "it comes to expanding his business and earning more money. The",
-        "cashier thinks he killed hiw own ally, the campsite owner, so that",
+        "cashier thinks he killed his own ally, the campsite owner, so that",
         "people would suspect the logger and the photographer, and so that",
         "he would get all the land to himself.",
         "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "Use the right and left arrows to navigate through the clues"
+    },
+    {
+        "According to some newspapers, the shop owner has a habit of taking",
+        "small businesses to court to expand his own company, even if it",
+        "means destroying a few lives.",
+        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "Use the right and left arrows to navigate through the clues"
+    },
+    {
+        "According to the fisherman, it's the 'monster of the lake' who killed",
+        "the campsite owner. The logger had made too much noise using his",
+        "chainswaw to whole afternoon, so the monster got angry and came out",
+        "of the water to kill the first person he saw.",
+        "The fisherman wouldn't talk about anything else than this monster.",
+        "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "Use the right and left arrows to navigate through the clues"
+    },
+    {
+        "According to a woman, when the lumberjack is cutting wood,",
+        "he doesn't notice people around him because of the noise and the",
+        "sawdust.",
+        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "Use the right and left arrows to navigate through the clues"
+    },
+    {
+        "The librarian is the logger's fiancé. Few people know because",
+        "Jack doesn't come from a wealthy family so the librarian's family",
+        "is firmly against this union.",
+        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "Use the right and left arrows to navigate through the clues"
+    },
+    {
+        "According to the librarian, she and her fiancé were at the bar when",
+        "the murder happened.",
+        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "Use the right and left arrows to navigate through the clues"
+    },
+    {
+        "According to a woman, the barman had to close his bar for the whole",
+        "afternoon 3 days ago.",
+        "Also, apparently your awesome socks make you look like a former",
+        "alcohol addict. Too flashy ?",
+        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "Use the right and left arrows to navigate through the clues"
+    },
+    {
+        "According to a child, the monster of the lake is the murderer. He",
+        "can't go play in the forest since what happened 3 days ago.",
+        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "Use the right and left arrows to navigate through the clues"
+    },
+    {
+        "According to a woman, Lily, who is the librarian, has a bike",
+        "which stopped working 4 days ago. She said she would go ask Jack",
+        "for help the next day.",
+        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "Use the right and left arrows to navigate through the clues"
+    },
+    {
+        "According to the policeman, letters from Mr Bezoz were found in",
+        "Rob's house after his death. In these letters, Mr Bezoz say they",
+        "should 'get rid of any difficulties', and the last letter, which",
+        "dates back to a week ago, says they should 'start taking actions",
+        "in a few days'.",
+        "The policeman thinks they intended to kill Jack and Matt.",
+        "", "", "", "", "", "", "", "", "", "", "", "", "",
         "Use the right and left arrows to navigate through the clues"
     }
 };
